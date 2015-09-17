0149c7ab4c9f675926559b5d60d58918abea583b
DeAndre Johnson <dre@phreshr.com>
2015-09-16 23:15:17 -0400

    updated component generator

--- generator/component/temp.js
+++ generator/component/temp.js
@@ -1,10 +1,41 @@
-import angular from 'angular';
-import 'angular-ui-router';
-import <%= name %>Component from './<%= name %>.component';
+import template from './<%= name %>.html!text';
+import {RouteConfig, Component, View, Inject} from '../../core/decorators/decorators';
 
-const <%= name %>Module = angular.module('<%= name %>', [
-  'ui.router'
-])
-.directive('<%= name %>', <%= name %>Component);
+// start-non-standard
+@RouteConfig('app.<%= name %>', {
+  url: '/',
+  template: '<<%= name %>></<%= name %>>',
+  resolve: {
+    // Constant Meta
+    $title: () => '<%= upCaseName %>',
+    $description: () => '<%= upCaseName %> description'
+  }
+})
+@Component({
+  selector: '<%= name %>'
+})
+@View({
+  template: template
+})
+@Inject('$log')
+// end-non-standard
 
-export default <%= name %>Module;
+// <%= upCaseName %> Controller
+class <%= upCaseName %> {
+  constructor() {
+    this.name = '<%= name %>';
+    this.activated = false;
+    // On load
+    this.activate();
+  }
+
+  /**
+   * Handles on load processing, and loading initial data
+ */
+  activate() {
+
+    this.activated = true;
+  }
+}
+
+export default <%= upCaseName %>;
